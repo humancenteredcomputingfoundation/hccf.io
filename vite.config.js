135b7528e8616ded051a6ed32095ea7304a2b962
edvv1n <edvvinudofia@gmail.com>
2026-09-04 23:27:15 +0100

    Updated p.JASON

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
+// https://vite.dev/config/
 export default defineConfig({
+  base: '/hccf.io/', // Replace <repository-name> with your GitHub repo name
   plugins: [react()],
-  base: '/hccf.io/',
-})
+});
